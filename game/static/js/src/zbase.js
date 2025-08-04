95b4180e9c3e5914844a12bbe40d1abf91617d30
xiaoxin <3346885079@qq.com>
2025-08-04 14:05:56 +0800

    write menu and playground scene

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,5 +1,14 @@
 class XiaoxinGame{
     constructor(id) {
+        this.id = id;
+        this.$xiaoxin_game = $('#' + id);
+        this.menu = new XiaoxinGameMenu(this);
+        this.playground = new XiaoxinGamePlayground(this);
 
+        this.start();
     }
+
+    start(){
+    }
+
 }
