1a83fc888ea80b23a87b693e50b2f91ec30c0338
xiaoxin <3346885079@qq.com>
2025-08-09 16:01:50 +0800

    finish move function

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -7,7 +7,8 @@ class XiaoxinGamePlayground{
         this.width = this.$playground.width();
         this.height = this.$playground.height();
         this.game_map = new GameMap(this);
-
+        this.players = [];
+        this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.15, true));
         // this.hide();
 
         this.start();
